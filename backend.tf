3675e9ab2f7997a811405831ebad2c23f2787a46
sriramarji <52248143+sriramarji@users.noreply.github.com>
2025-06-09 06:40:26 +0000

    updated backend.tf

--- backend.tf
+++ backend.tf
@@ -1,9 +1,9 @@
-/*terraform {
+terraform {
   backend "s3" {
     bucket       = "tf-bucket-prod1"
-    key          = "uc6-inst-scheduler/terraform.tfstate"
+    key          = "uc7-lambda-container/terraform.tfstate"
     region       = "us-east-1"
     use_lockfile = true
     encrypt      = true
   }
-}*/
+}
