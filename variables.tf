ffe1c4f0d406bc0cd3da12f7fc5261c706cbd609
sriramarji <52248143+sriramarji@users.noreply.github.com>
2025-06-09 06:01:53 +0000

    added variables.tf

--- variables.tf
+++ variables.tf
@@ -1,18 +1,22 @@
 variable "vpc_cidr" {
   description = "CIDR block for the VPC"
   type        = string
+  default = "192.168.0.0/16"
 }
 variable "pub_sub_count" {
   description = "Number of public subnets"
   type        = number
+  default = 2
 }
 
 variable "priv_sub_count" {
   description = "Number of private subnets"
   type        = number
+  default = 2
 }
 
 variable "nat_count" {
   description = "Number of NAT gateways"
   type        = number
+  default = 1
 }
